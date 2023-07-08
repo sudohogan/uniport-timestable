142da255346f1e1ca9464ce0681cac2a57647e65
Miracle Jude <judemiraco0@gmail.com>
2023-07-08 10:17:19 +0100

    added addcourse and authorization

--- src/components/CourseModal.tsx
+++ src/components/CourseModal.tsx
@@ -2,23 +2,51 @@ import React from 'react'
 import { Input } from './ui/input';
 import { cn } from '@/lib/utils';
 import { buttonVariants } from './ui/button';
+import { Textarea } from './ui/textarea';
+
 const CourseModal: React.FC<{
     isOpen: boolean;
     onClose: () => void;
     onAddCourse: () => void;
     inputValue: string;
     setInputValue: React.Dispatch<React.SetStateAction<string>>;
-  }> = ({ isOpen, onClose, onAddCourse, inputValue, setInputValue }) => {
+    venueValue: string;
+    setVenueValue: React.Dispatch<React.SetStateAction<string>>;
+    lecturerValue: string;
+    setLecturerValue: React.Dispatch<React.SetStateAction<string>>;
+  }> = ({
+    isOpen,
+    onClose,
+    onAddCourse,
+    inputValue,
+    setInputValue,
+    venueValue,
+    setVenueValue,
+    lecturerValue,
+    setLecturerValue,
+  }) => {
     if (!isOpen) return null;
     
     return (
       <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 bg-gray-500 p-4">
         <div className="bg-gray-900 p-4 rounded-md grid gap-4 w-full max-w-[400px]">
           <Input
-            placeholder="Enter Course"
+            placeholder="Enter course"
             type="text"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
+          />
+            <Input
+              placeholder="Enter venue"
+              type="text"
+              value={venueValue}
+              onChange={(e) => setVenueValue(e.target.value)}
+          />
+          <Textarea
+            placeholder="Enter lecturer's name"
+            value={lecturerValue}
+            className="resize-none bg-gray-900"
+            onChange={(e) => setLecturerValue(e.target.value)}
           />
           <div className="justify-end flex items-center gap-3">
             <button
@@ -28,7 +56,7 @@ const CourseModal: React.FC<{
                 onClose();
               }}
             >
-              Add
+              Add class
             </button>
             <button
               className={cn(buttonVariants({variant: "destructive",}))}
--- src/components/CoursePreviewModal.tsx
+++ src/components/CoursePreviewModal.tsx
@@ -3,26 +3,31 @@ import React from 'react';
 const CoursePreviewModal: React.FC<{
     isOpen: boolean;
     onClose: () => void;
-    onCancel: () => void;
     course: string;
-  }> = ({ isOpen, onClose, course, onCancel }) => {
+    venue: string;
+    lecturers?: string[]
+  }> = ({ isOpen, onClose, course, venue, lecturers }) => {
     if (!isOpen) return null;
   
     return (
       <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 bg-gray-500 p-4">
         <div className="bg-gray-900 p-4 rounded-md w-full max-w-[400px]">
-          <h2 className="text-xl mb-3">Course Preview</h2>
-          <p>{course}</p>
-          <div className="flex items-center justify-end gap-2">
-            <button
-              className="px-2 ml-2 text-white bg-gray-500 rounded"
-              onClick={() => {
-                onCancel()
-                onClose()
-              }}
-            >
-              Cancel
-            </button>
+          <h2 className="text-2xl mb-3">Course Preview</h2>
+          <p className="font-semibold text-xl">{course}</p>
+          <p>{venue}</p>
+          {lecturers && lecturers.length > 0 && (
+            <div className="flex items-center justify-end mb-4">
+              <div>
+                <p>Lecturers:</p>
+                <ul className="list-none ml-6">
+                  {lecturers.map((lecturer, index) => (
+                    <li key={index}>{lecturer}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          )}
+          <div className="flex items-center justify-end">
             <button
               className="px-2 ml-2 text-white bg-red-500 rounded"
               onClick={onClose}
