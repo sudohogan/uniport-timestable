9fa88159e1c121e25225b4d921df8abdc64dac4f
Miracle Jude <judemiraco0@gmail.com>
2023-07-08 10:18:27 +0100

    update data

--- src/components/data.ts
+++ src/components/data.ts
@@ -3,13 +3,27 @@ export const Data = [
     {
       day: 'Monday',
       schedule: [
-        { time: '8AM - 9AM', course: 'CSC 483.1', venue: "mbs 22", lecturer: "Dr Oghenekaro" },
-        { time: '9AM - 10AM', course: 'CSC 483.1' },
+        { time: '8AM - 9AM', course: 'CSC 483.1', venue: "mbs 22", lecturers: [
+          "Dr. E.E. Ogheneovo",
+          "Dr. R. S. Ogunsakin",
+          "Dr. C. B. Marcus"
+        ] },
+        { time: '9AM - 10AM', course: 'CSC 483.1', venue: "mbs 22", lecturers: [
+          "Dr. E.E. Ogheneovo",
+          "Dr. R. S. Ogunsakin",
+          "Dr. C. B. Marcus"
+        ] },
         { time: '10AM - 11AM', course: '' },
         { time: '11AM - 12PM', course: '' },
         { time: '12PM - 1PM', course: '' },
-        { time: '1PM - 2PM', course: 'CSC 486.1' },
-        { time: '2PM - 3PM', course: 'CSC 486.1' },
+        { time: '1PM - 2PM', course: 'CSC 486.1', venue: "CSC hall 2", lecturers: [
+          "Dr. U. A. Okengwu",
+          "Mr. E. Wobidi",
+        ] },
+        { time: '2PM - 3PM', course: 'CSC 486.1', venue: "CSC hall 2", lecturers: [
+          "Dr. U. A. Okengwu",
+          "Mr. E. Wobidi",
+        ] },
         { time: '3PM - 4PM', course: '' },
         { time: '4PM - 5PM', course: '' },
         { time: '5PM - 6PM', course: '' },
@@ -18,12 +32,26 @@ export const Data = [
     {
       day: 'Tuesday',
       schedule: [
-        { time: '8AM - 9AM', course: 'CSC 480.1' },
-        { time: '9AM - 10AM', course: 'CSC 482.1' },
-        { time: '10AM - 11AM', course: 'CSC 482.1' },
+        { time: '8AM - 9AM', course: 'CSC 480.1', venue: "CSC hall 2", lecturers: [
+          "Prof. P.O. Asagb",
+          "Dr. M.O Musa",
+          "Mr. E. Wobidi"
+        ] },
+        { time: '9AM - 10AM', course: 'CSC 482.1', venue: "mbs 22", lecturers: [
+          "Mr. E. Wobidi",
+          "Prof. C. Ugwu",
+        ] },
+        { time: '10AM - 11AM', course: 'CSC 482.1', venue: "mbs 22", lecturers: [
+          "Mr. E. Wobidi",
+          "Prof. C. Ugwu",
+        ] },
         { time: '11AM - 12PM', course: '' },
         { time: '12PM - 1PM', course: '' },
-        { time: '1PM - 2PM', course: 'CSC 483.1' },
+        { time: '1PM - 2PM', course: 'CSC 483.1', venue: "CSC hall 2", lecturers: [
+          "Dr. E.E. Ogheneovo",
+          "Dr. R. S. Ogunsakin",
+          "Dr. C. B. Marcus"
+        ] },
         { time: '2PM - 3PM', course: '' },
         { time: '3PM - 4PM', course: '' },
         { time: '4PM - 5PM', course: '' },
@@ -37,10 +65,25 @@ export const Data = [
         { time: '9AM - 10AM', course: '' },
         { time: '10AM - 11AM', course: '' },
         { time: '11AM - 12PM', course: '' },
-        { time: '12PM - 1PM', course: 'CSC 498.1' },
-        { time: '1PM - 2PM', course: 'CSC 480.1' },
-        { time: '2PM - 3PM', course: 'CSC 480.1' },
-        { time: '3PM - 4PM', course: 'GES 400.1' },
+        { time: '12PM - 1PM', course: 'CSC 498.1', venue: "CSC hall 2", lecturers: [
+          "Prof. B.O. Eke",
+          "Mr. A.O. Ugbari",
+        ] },
+        { time: '1PM - 2PM', course: 'CSC 480.1', venue: "CSC hall 2", lecturers: [
+          "Prof. P.O. Asagba",
+          "Dr. M.O Musa",
+          "Mr. E. Wobidi"
+        ] },
+        { time: '2PM - 3PM', course: 'CSC 480.1', venue: "CSC hall 2", lecturers: [
+          "Prof. P.O. Asagba",
+          "Dr. M.O Musa",
+          "Mr. E. Wobidi"
+        ] },
+        { time: '3PM - 4PM', course: 'GES 400.1', venue: "CSC hall 2", lecturers: [
+          "Dr. F. E. Onuodu",
+          "Dr. U.A. Okengwu",
+          "Dr. P. Enyindah"
+        ] },
         { time: '4PM - 5PM', course: '' },
         { time: '5PM - 6PM', course: '' },
       ],
@@ -50,28 +93,58 @@ export const Data = [
       schedule: [
         { time: '8AM - 9AM', course: '' },
         { time: '9AM - 10AM', course: '' },
-        { time: '10AM - 11AM', course: 'CSC 496.1' },
-        { time: '11AM - 12PM', course: 'CSC 481.1' },
+        { time: '10AM - 11AM', course: 'CSC 496.1', venue: "mbs 15", lecturers: [
+          "Prof. L. N. Onyejegbu,",
+          "Dr. L.U. Oghenekaro",
+          "Dr. U.A. Okengwu"
+        ] },
+        { time: '11AM - 12PM', course: 'CSC 481.1', venue: "mbs 22", lecturers: [
+          "Dr. B.B. Baridam",
+          "Dr. L.U. Oghenekaro",
+          "Dr. L. C. Ochei"
+        ] },
         { time: '12PM - 1PM', course: '' },
         { time: '1PM - 2PM', course: '' },
         { time: '2PM - 3PM', course: '' },
         { time: '3PM - 4PM', course: '' },
         { time: '4PM - 5PM', course: '' },
-        { time: '5PM - 6PM', course: 'CSC 482.1' },
+        { time: '5PM - 6PM', course: 'CSC 482.1', venue: "CSC hall 2", lecturers: [
+          "Mr. E. Wobidi",
+          "Prof. C. Ugwu",
+        ] },
       ],
     },
     {
       day: 'Friday',
       schedule: [
         { time: '8AM - 9AM', course: '' },
-        { time: '9AM - 10AM', course: 'CSC 486.1' },
-        { time: '10AM - 11AM', course: 'CSC 498.1' },
-        { time: '11AM - 12PM', course: 'CSC 496.1' },
-        { time: '12PM - 1PM', course: 'CSC 481.1' },
+        { time: '9AM - 10AM', course: 'CSC 486.1', venue: "CSC hall 2", lecturer: [
+          "Dr. U. A. Okengwu",
+          "Mr. E. Wobidi",
+        ] },
+        { time: '10AM - 11AM', course: 'CSC 498.1', venue: "CSC hall 2", lecturer: [
+          "Dr Oghenekaro",
+          "Dr Oghenekaro",
+          "Dr Oghenekaro"
+        ] },
+        { time: '11AM - 12PM', course: 'CSC 496.1', venue: "CSC hall 2", lecturer: [
+          "Prof. L. N. Onyejegbu,",
+          "Dr. L.U. Oghenekaro",
+          "Dr. U.A. Okengwu"
+        ] },
+        { time: '12PM - 1PM', course: 'CSC 481.1', venue: "CSC hall 2", lecturer: [
+          "Dr. B.B. Baridam",
+          "Dr. L.U. Oghenekaro",
+          "Dr. L. C. Ochei"
+        ] },
         { time: '1PM - 2PM', course: '' },
         { time: '2PM - 3PM', course: '' },
         { time: '3PM - 4PM', course: '' },
-        { time: '4PM - 5PM', course: 'GES 400.1' },
+        { time: '4PM - 5PM', course: 'GES 400.1', venue: "mbs 22", lecturer: [
+          "Dr. F. E. Onuodu",
+          "Dr. U.A. Okengwu",
+          "Dr. P. Enyindah"
+        ] },
         { time: '5PM - 6PM', course: '' },
       ],
     },
